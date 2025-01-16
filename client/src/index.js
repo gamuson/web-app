e1c58f3c1618ed175fe902b4c13bdb4210ca794c
gamuson <danicc_2023@hotmail.com>
2025-01-16 21:38:52 +0100

    client problems fixed

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,12 @@
-// **client/src/index.js**
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
 
-const logger = require('./logger');
-const cors = require('cors');
-
-app.use(cors({
-  origin: 'http://localhost:5173', // URL del frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-}));
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-logger.info('Montando la aplicación React en el elemento #root');
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );

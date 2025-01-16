c00d078160d00a9f8fb30d2d028d08afcdf29b21
gamuson <danicc_2023@hotmail.com>
2025-01-16 12:18:38 +0100

    Moved global.css import to index.js and adjusted project structure

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/global.css';
+
 const logger = require('./logger');
 const cors = require('cors');
 
